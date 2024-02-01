e19f6bd4b57b60051748b2cf0f59be5e06f4fbd1
bivouac95 <tivan0408@gmail.com>
2024-02-01 14:21:21 +0900

    5.1

---
+++ 5/1/index.js
@@ -0,0 +1,11 @@
+const existedUserLogin = 'the_best_user';
+const existedUserPassword = '12345678';
+
+let login = prompt('Введите логин').trim();
+let password = prompt('Введите пароль').trim();
+
+if (login == existedUserLogin && password == existedUserPassword){
+    alert(`Добро пожаловать ${login}!`);
+} else {
+    alert('Неправильный логин и (или) пароль');
+}
